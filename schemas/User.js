b9acbb72c599b323935b329c396a6b287cd8fe1e
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-09-14 23:28:46 +0430

    feat: Building the User schema

---
+++ schemas/User.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const schema = {
+  email: Joi.string().min(5).max(50).email().required(),
+  password: Joi.string().min(8).max(50).required(),
+};
+
+const UserSchema = Joi.object({
+  firstName: Joi.string()
+    .pattern(/^[A-Z|a-z|\s]+$/)
+    .min(2)
+    .max(50)
+    .required(),
+  lastName: Joi.string()
+    .pattern(/^[A-Z|a-z|\s]+$/)
+    .min(2)
+    .max(50)
+    .required(),
+  ...schema,
+});
+
+const LoginSchema = Joi.object(schema);
+
+exports.UserSchema = UserSchema;
+exports.LoginSchema = LoginSchema;
