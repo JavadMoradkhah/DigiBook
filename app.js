62d508a95254813b0fbc6415f42d2150e2f78f76
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-09-14 21:57:48 +0430

    feat: Setup mysql database

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const homeRoute = require('./routes/home');
+const database = require('./startup/database');
+
+database
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,4 +21,8 @@ app.use('/api/', homeRoute);
 
 app.listen(port, () => {
   console.log(`Server is running: http://localhost:${port}/api/`);
+});
+
+process.on('exit', () => {
+  database.close();
 });
