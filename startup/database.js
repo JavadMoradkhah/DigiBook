dc46c1899c27beb029e1069dd4b14e07abf839b6
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-09-16 19:18:29 +0430

    feat: Logging database logs into a log file.

--- startup/database.js
+++ startup/database.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require('sequelize');
+const { writeFile } = require('fs').promises;
 
 const { DB_HOST, DB_NAME, DB_USER, DB_PASS } = process.env;
 
 const database = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: 'mysql',
+  logging: async (msg) => {
+    await writeFile('db.log', `${msg}\n`, { flag: 'a' });
+  },
 });
 
 module.exports = database;
