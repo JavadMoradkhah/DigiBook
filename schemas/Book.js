864482f96a0582141f6b7e8a11920ab41df98c91
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-09-16 19:21:04 +0430

    refactor: Book schema

--- schemas/Book.js
+++ schemas/Book.js
@@ -7,7 +7,6 @@ const schema = {
     .max(255),
   genre_id: Joi.number().integer(),
   description: Joi.string().min(5).max(2000),
-  thumbnail_image: Joi.string().min(5).max(255),
   author: Joi.string()
     .pattern(/^[A-Z|a-z|\s]+$/)
     .min(3)
@@ -27,7 +26,6 @@ exports.BookSchema = Joi.object({
   title: schema.title.required(),
   genre_id: schema.genre_id.required(),
   description: schema.description.required(),
-  thumbnail_image: schema.thumbnail_image.required(),
   author: schema.author.required(),
   publisher: schema.publisher,
   copyright_holder: schema.copyright_holder,
