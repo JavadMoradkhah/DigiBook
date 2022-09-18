f17e979be3e59e58753b728d7c56e7fd88124a61
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-09-18 19:23:54 +0430

    feat: Deleting uploaded file when an error occurred in error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,17 @@
+const path = require('path');
+const { unlink } = require('fs').promises;
 const AppError = require('../utils/AppError');
 
-module.exports = (err, req, res, next) => {
+module.exports = async (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     console.log(err);
+  }
+
+  // Checking if a file has been uploaded
+  if (req.file) {
+    const uploadedFilePath = path.resolve(process.cwd(), `./public/uploads/${req.file.filename}`);
+    // Deleting uploaded file because of occurring error
+    await unlink(uploadedFilePath);
   }
 
   const defaultErrorMessage = 'Something went wrong: Please try again later.';
--- middleware/validators/book.js
+++ middleware/validators/book.js
@@ -1,22 +1,17 @@
-const path = require('path');
-const { unlink } = require('fs').promises;
+const AppError = require('../../utils/AppError');
 const Genre = require('../../models/Genre');
 const { BookSchema } = require('../../schemas/Book');
 
 module.exports = async (req, res, next) => {
   try {
-    const uploadedFilePath = path.resolve(process.cwd(), `./public/uploads/${req.file.filename}`);
-
     const validation = BookSchema.validate(req.body);
     if (validation.error) {
-      await unlink(uploadedFilePath);
-      return res.status(400).json({ status: 'fail', message: validation.error.message });
+      return next(new AppError(400, 'fail', validation.error.message));
     }
 
     const genre = await Genre.findByPk(req.body.genre_id);
     if (!genre) {
-      await unlink(uploadedFilePath);
-      return res.status(400).json({ status: 'fail', message: 'Invalid genre Id.' });
+      return next(new AppError(400, 'fail', 'Invalid genre Id.'));
     }
 
     next();
