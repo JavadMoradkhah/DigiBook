1fadb85fd3c01d1e7179d3b2d10717fc440df8d1
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-10-04 19:10:39 +0330

    refactor: Adding User update schema

--- schemas/User.js
+++ schemas/User.js
@@ -1,26 +1,33 @@
 /* eslint-disable newline-per-chained-call */
 const Joi = require('joi');
 
-const schema = {
-  email: Joi.string().min(5).max(50).email().required(),
-  password: Joi.string().min(8).max(50).required(),
+const fields = {
+  name: Joi.string()
+    .pattern(/^[A-Z|a-z|\s]+$/)
+    .min(2)
+    .max(50),
+  email: Joi.string().min(5).max(50).email(),
+  password: Joi.string().min(8).max(50),
 };
 
 const UserSchema = Joi.object({
-  first_name: Joi.string()
-    .pattern(/^[A-Z|a-z|\s]+$/)
-    .min(2)
-    .max(50)
-    .required(),
-  last_name: Joi.string()
-    .pattern(/^[A-Z|a-z|\s]+$/)
-    .min(2)
-    .max(50)
-    .required(),
-  ...schema,
+  first_name: fields.name.required(),
+  last_name: fields.name.required(),
+  email: fields.email.required(),
+  password: fields.password.required(),
 });
 
-const LoginSchema = Joi.object(schema);
+const LoginSchema = Joi.object({
+  email: fields.email.required(),
+  password: fields.password.required(),
+});
+
+const UserUpdateSchema = Joi.object({
+  first_name: fields.name,
+  last_name: fields.name,
+  password: fields.password,
+});
 
 exports.UserSchema = UserSchema;
 exports.LoginSchema = LoginSchema;
+exports.UserUpdateSchema = UserUpdateSchema;
